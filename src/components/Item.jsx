bf36a876b3d0780ece1cc070f37d3d9c89a5c0a5
Juan Esteban Gomez <juanegomez5@gmail.com>
2022-06-23 23:24:38 -0300

    Routing v2.1

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 const Item = ({item}) => {
   return (
-    <div className="item" style={{ margin: '10px 20px', border: '2px solid black' }}>
+    <div className="item" style={{ margin: '10px 20px', border: '1px solid black' }}>
       <h4>{item.title}</h4>
       <img src={item.image} alt="thumbnail" width="200"/>
       <p>Precio : ${item.price}</p>
